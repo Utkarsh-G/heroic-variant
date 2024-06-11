9d9e1b2a433575e67ce7bad7a6003b72bb6090bf
Utkarsh Gaur <charmedtobe@gmail.com>
2024-06-10 22:35:27 -0400

    Automating the bonus from unsettled injuries

--- hooks.js
+++ hooks.js
@@ -89,18 +89,17 @@ const _getEntryContextOptions_Wrapper = (wrapped) => {
       condition: canKeelyHeroPointReroll,
       callback:  li => {
         const message = game.messages.get(li[0].dataset.messageId, {strict: true});
-
+        const messageActor = message.actor;
+        const actor = messageActor?.isOfType("familiar") ? messageActor.master : messageActor;
         const tempHook = Hooks.on('pf2e.reroll', pf2eRerollHook);
 
         game.pf2e.Check.rerollFromMessage(message, {heroPoint: true}).then(() => {
-          Hooks.off('pf2e.reroll', tempHook)
+          Hooks.off('pf2e.reroll', tempHook);
         })
-        const messageActor = message.actor;
-        const actor = messageActor?.isOfType("familiar") ? messageActor.master : messageActor;
+
         const newValue = actor.heroPoints.value - 2;
         actor.update({'system.resources.heroPoints.value': newValue}).then() // clamp to min 0? handle returned promise?
 
-        
       },
     },
     {
@@ -111,15 +110,61 @@ const _getEntryContextOptions_Wrapper = (wrapped) => {
         if (!_token || !_token.actor || !_token.actor.heroPoints){console.log("No selected token for hero point source."); return;}
         if (_token.actor.heroPoints.value > 1) {
           const message = game.messages.get(li[0].dataset.messageId, {strict: true});
-          game.pf2e.Check.rerollFromMessage(message, {heroPoint: false, keep: 'new'}).then(() => {})
+          game.pf2e.Check.rerollFromMessage(message, {heroPoint: false, keep: 'new'}).then(() => {
+          })
           const newValue = _token.actor.heroPoints.value - 2;
-          _token.actor.update({'system.resources.heroPoints.value': newValue}).then() // clamp to min 0? handle returned promise?
+          _token.actor.update({'system.resources.heroPoints.value': newValue}).then() // clamp would still be nice.
         }
       },
     }
   )
   return buttons
 }
+
+// Modified from github.com/xdy/xdy-pf2e-workbench
+function pf2eRerollHookWithUnsettledInjuries(
+  _oldRoll,
+  newRoll,
+  heroPoint,
+  keep, // : "new" | "higher" | "lower",
+) {
+  if (keep !== "new" || !_token || !_token.actor) return;
+  const hasUnsettledInjuries = !!_token.actor.itemTypes.effect.find(effect => effect.name === "Unsettled Injuries");
+
+  // @ts-ignore
+  if (hasUnsettledInjuries && heroPoint){
+    const die = newRoll.dice.find((d) => d instanceof Die && d.number === 1 && d.faces === 20);
+    if (die) {
+      newRoll.terms.push(
+          // @ts-ignore
+          OperatorTerm.fromData({ class: "OperatorTerm", operator: "+", evaluated: true }),
+          // @ts-ignore
+          NumericTerm.fromData({ class: "NumericTerm", number: 1, evaluated: true }),
+      );
+      // @ts-ignore It's protected. Meh.
+      newRoll._total += 1;
+      newRoll.options.hasUnsettledInjuries = true;
+    }
+  }
+
+  if (hasUnsettledInjuries && !heroPoint){
+    const die = newRoll.dice.find((d) => d instanceof Die && d.number === 1 && d.faces === 20);
+    if (die) {
+      newRoll.terms.push(
+          // @ts-ignore
+          OperatorTerm.fromData({ class: "OperatorTerm", operator: "-", evaluated: true }),
+          // @ts-ignore
+          NumericTerm.fromData({ class: "NumericTerm", number: 1, evaluated: true }),
+      );
+      // @ts-ignore It's protected. Meh.
+      newRoll._total -= 1;
+      newRoll.options.enemyHasUnsettledInjuries = true;
+    }
+  }
+
+}
+
+Hooks.on("pf2e.reroll", pf2eRerollHookWithUnsettledInjuries);
 
 // Code copied from github.com/xdy/xdy-pf2e-workbench
 function pf2eRerollHook(
@@ -147,6 +192,7 @@ function pf2eRerollHook(
 }
 
 // Code modified from github.com/xdy/xdy-pf2e-workbench
+// massive repetition
 function renderChatMessageHook(message, jq) {
   const html = jq.get(0);
 
@@ -175,6 +221,74 @@ function renderChatMessageHook(message, jq) {
             const span = document.createElement("span");
             span.className = "keeley-add-10";
             span.innerText = " + 10";
+            formulaElem?.append(span);
+            formulaElem.style.color = "darkblue";
+
+            // Make the total purple
+            newTotalElem.classList.add("keeley-add-10");
+            newTotalElem.style.color = "darkblue";
+        }
+    }
+  }
+
+  if (lastRoll?.options.hasUnsettledInjuries) {
+    const element = jq.get(0);
+
+    if (element) {
+        const tags = element.querySelector(".flavor-text > .tags.modifiers");
+        const formulaElem = element.querySelector(".reroll-discard .dice-formula");
+        const newTotalElem = element.querySelector(".reroll-second .dice-total");
+        if (tags && formulaElem && newTotalElem) {
+            // Add a tag to the list of modifiers
+            const newTag = document.createElement("span");
+            newTag.classList.add("tag", "tag_transparent", "keeley-add-10");
+            newTag.innerText = 'Unsettled Injuries Reroll +1';
+            newTag.dataset.slug = "keeley-add-10";
+            newTag.style.color = "darkblue";
+            const querySelector = tags.querySelector(".tag");
+            if (querySelector?.dataset.visibility === "gm") {
+                newTag.dataset.visibility = "gm";
+            }
+            tags.append(newTag);
+
+            // Show +10 in the formula
+            const span = document.createElement("span");
+            span.className = "keeley-add-10";
+            span.innerText = " + 1";
+            formulaElem?.append(span);
+            formulaElem.style.color = "darkblue";
+
+            // Make the total purple
+            newTotalElem.classList.add("keeley-add-10");
+            newTotalElem.style.color = "darkblue";
+        }
+    }
+  }
+
+  if (lastRoll?.options.enemyHasUnsettledInjuries) {
+    const element = jq.get(0);
+
+    if (element) {
+        const tags = element.querySelector(".flavor-text > .tags.modifiers");
+        const formulaElem = element.querySelector(".reroll-discard .dice-formula");
+        const newTotalElem = element.querySelector(".reroll-second .dice-total");
+        if (tags && formulaElem && newTotalElem) {
+            // Add a tag to the list of modifiers
+            const newTag = document.createElement("span");
+            newTag.classList.add("tag", "tag_transparent", "keeley-add-10");
+            newTag.innerText = 'Unsettled Injuries Reroll -1';
+            newTag.dataset.slug = "keeley-add-10";
+            newTag.style.color = "darkblue";
+            const querySelector = tags.querySelector(".tag");
+            if (querySelector?.dataset.visibility === "gm") {
+                newTag.dataset.visibility = "gm";
+            }
+            tags.append(newTag);
+
+            // Show +10 in the formula
+            const span = document.createElement("span");
+            span.className = "keeley-add-10";
+            span.innerText = " - 1";
             formulaElem?.append(span);
             formulaElem.style.color = "darkblue";
 
